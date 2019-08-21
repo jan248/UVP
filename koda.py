b09b1c3a4957d7ed32ceac8519fe1353eb87ecc3
jan248 <45061111+jan248@users.noreply.github.com>
2019-08-21 21:52:13 +0200

    Update koda.py

--- koda.py
+++ koda.py
@@ -25,16 +25,17 @@ def converter():
     except ValueError:
         return bottle.template("index.html")
 
-
 @bottle.post('/past_converter/')
 def converter():
     print(bottle.request)
     value = float(bottle.request.forms['value'])
     from_currency = bottle.request.forms['from_currency']
     to_currency = bottle.request.forms['to_currency']
+    past_date = bottle.request.forms['past_date']
+    my_date = datetime.datetime.strptime(past_date, '%Y-%m-%d')
     try:
         tecaj = get_tecaj(from_currency, to_currency)
-        final_value = c.convert(value, from_currency, to_currency)
+        final_value = c.convert(value, from_currency, to_currency, my_date)
         return bottle.template('result.html', final_value=final_value, value=value,
                                from_currency=from_currency, to_currency=to_currency, tecaj=tecaj)
     except ValueError:
